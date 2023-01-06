ab08d2bd1920f89d4b8a5e6ec162aeecffdaf6c7
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-01-06 21:37:51 +0530

    day two
    what are variables
    how to use them
    difference b/w var/let/const

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
+/**to diaply the text */
 console.log("hello world");
+console.log("Hello Myself uma");
+
+/**to add numbers to diaply result */
+console.log(7+3);
---
+++ variables.js
@@ -0,0 +1,30 @@
+/**variables  
+ * variable=placeholder 
+*/
+/**craete a variable */
+var name;
+/**store info in var */
+name ="Uma Mina";
+/**diaply the text */
+console.log(name);
+/***chaning varibale */
+name ="neha minaaaaa";
+console.log(name);
+
+/**declaring a variable */
+let message;
+/***assigning value to varibale */
+ message = "hello this is my first program";
+ /***printing name */
+ console.log(message);
+
+ let text = "I am enjoying doing this"
+ console.log(text);
+
+
+
+ /**const is used for constant values */
+
+ const number = 4;
+
+ console.log(number);
