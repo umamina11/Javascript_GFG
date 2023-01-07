d4202d0bb8381da20d072e3ba52743563ed46f01
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-01-07 17:45:54 +0530

    day 3
    concentration and literals
    basic arthematic operators

---
+++ .vscode/arthematicoperators.js
@@ -0,0 +1,43 @@
+  const x= 12;
+  const y = 12;
+  /** Basic operations */
+/**add */
+  console.log(x+y);
+  /**substract */
+  console.log(x-y);
+  /**multiply */
+console.log(x*y);
+  /**divide */
+console.log(x/y);
+
+/** modolus
+ * 
+*/
+console.log(x%y);
+
+
+console.log(x ** 2);   /// calculating the square of the x
+
+console.log(x ** 3);   /// calculating the cube of the x
+/** when varibles acts as string */
+let a = "2";
+let b = "4";
+
+console.log(a+b);  // it will concenate the number and rest operators will convert them in numeric internally
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**2);
+console.log(a**3);
+
+/**when one variable is string and another one is number  */
+ let p =" 5";
+ let q = 2;
+ console.log(p+q);
+ console.log(p-q);
+ console.log(p*q);
+ console.log(p/q);
+ console.log(p%q);
+ console.log(p**3);
+ console.log(p**2);
