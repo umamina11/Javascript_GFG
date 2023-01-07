1d29a92585fba3c4013c45bd5a92c6b4deaed89e
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-01-07 22:27:35 +0530

    Update readuserdata.js

--- .vscode/readuserdata.js
+++ .vscode/readuserdata.js
@@ -1,4 +1,5 @@
 /** read data from user */
-const readlineSync = require("readline-sync");  // inasatinf the module
+const readlineSync = require("readline-sync");  
 const userName = readlineSync.question("what is user name?")
-console.log( "welcome " + userName);
+console.log( "welcome " + userName); // using concationation
+console.log('welcome ${userName}');
