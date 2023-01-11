981b1679f00a42e769aa6d1920874b0674d06a53
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-01-11 14:50:06 +0530

    conditional statement_ 1

---
+++ conditional_statements.js
@@ -0,0 +1,37 @@
+/** condititonal statements
+ * 
+SYNTAX
+if (condition/ expression ){
+    body/ action tobe performed
+}
+
+
+
+condition is true it will print statement and if false not give any output
+ */
+
+if (true){
+    console.log("hello myself uma");      // this statement is true and will give the output at the console
+
+}
+
+if (false){
+    console.log(" are u going with me?");     // here conditon is false so this expression will not give any output
+
+}
+
+const is_login = true;
+
+if (is_login){
+    console.log("I am logged in");           // the variable value is true therefore it will print the output 
+}
+
+const islogin = false;                      // the variable va;ue is false therefore it will not give any output
+
+if (islogin){
+    console.log("I am logged in");
+}
+
+
+
+
