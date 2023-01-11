418b44ea85b40b08f98e3cc610402d481d8cfe93
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-01-11 11:12:51 +0530

    Comparison operators

---
+++ comparision.js
@@ -0,0 +1,33 @@
+/** comparision operators
+ * 
+ */
+/** return true if statement is true and false if the expression is wrong */
+console.log(12>23);
+
+console.log(11>10);
+
+console.log(11>=11);  // cehck two condtions that number is greater than and then number is equal or not
+
+console.log(100==100); //values are eual or not
+console.log(199==100);
+
+
+
+
+/** comparision in strings */
+console.log("apple">"banana");   //comnapres the first charqacter wherther the numerical value for say Asiic values where 'a' value is smaller that  'b' so this expression is fasle 
+// if the value of the first character is equal is value then they move ti the next character 
+
+
+console.log("uma">"umamina");
+console.log("umamina">"uma"); // this is true as till a both are same so it move to nextr where in first string m is ther and second one it is missing thats why it is true
+
+console.log("uma">="umamina");
+
+
+
+
+/** comparision with string and a number */
+console.log("2"> 1);
+
+console.log("01"==1);
