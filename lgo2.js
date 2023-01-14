63098c33059edd6af75963457f7d08fcf98e7336
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-01-14 23:27:56 +0530

    logical operators

---
+++ lgo2.js
@@ -0,0 +1,39 @@
+/** logical operators part 2
+ * OR
+ * AND 
+ * turly 
+ * falsly: "", 0, null, undefined 
+ * 
+ */
+
+let firstname=" Uma";
+let secondname="Mina";
+const uname=firstname|| secondname;
+console.log(`Name:${uname}`);
+
+//console.log(firstname || secondname); 
+
+// trying with the undefined and the null values to see the results
+const fnum= null;
+const snum= undefined;
+console.log(fnum || snum);
+
+
+
+
+
+let a= 23;
+let b;
+console.log(a+b);// here the output will be NaN as b value is undefined
+// to deal this situation we provide a condition that is where if the b is not defined then take it 0
+console.log(a+(b||0));
+
+
+
+// using the AND operatror 
+let fname=" Uma";
+let sname="Mina";
+const username=fname && sname;
+console.log(`Name:${username}`); // here the output will give the sname beacuse the AND operator first look for the falsy value
+                                // irrespective truly values present or not it will always look for falsy value and get that at console
+                                
