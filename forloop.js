a81e4c2ed366cc06224c7e4bb80dce1320b8df52
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-01-15 22:07:05 +0530

    for loop

---
+++ forloop.js
@@ -0,0 +1,16 @@
+/** FOR LOOP 
+ * 
+ */
+
+/**syntax
+for(start_value; Condition; Incremental/Drecemental_Value ){
+    body
+}**/
+
+for(let i=0;i<10;i++){
+    console.log(i);
+    
+    console.log("hello world..");
+}
+
+// print the name in the 
