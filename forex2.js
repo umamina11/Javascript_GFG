0a09392eb9af3e0553156a4533a5267543ba0546
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-01-16 22:01:31 +0530

    exercise

---
+++ forex2.js
@@ -0,0 +1,13 @@
+//print the number of characters in the given string 
+
+const userName= "UMA MINA";
+
+let count =0;
+for(let i=0;i<userName.length; i++){
+    count++;
+}
+console.log( "number of characters in string is "+  count);
+
+
+// print the number even and odd in a given range of number
+
