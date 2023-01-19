6935de4a747d1f348afc852572654c5cd71d4a17
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-01-19 19:56:02 +0530

    simple function call

---
+++ greetmessage.js
@@ -0,0 +1,11 @@
+// accepting the input in the function to run the code
+
+function greet(name, city){                      // here name is called as an parameter
+    console.log(`${name} Hello! nice to meet you`);
+    console.log(`I live in ${city}`);
+}
+
+greet("John","Jaipur");     // here john is called as an argument(or the value of the parameter)
+greet("Priya","Bihar");
+greet("Kivi","Delhi");
+greet("Uma")                     // at palce of city the output given is undefiend
