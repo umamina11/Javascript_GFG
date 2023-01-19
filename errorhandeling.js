2147e9cbe15b39eae42e6c38ae54ed83f11bc7b8
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-01-19 18:19:37 +0530

    errorhandeling

--- errorhandeling.js
+++ errorhandeling.js
@@ -12,8 +12,12 @@ try {
     someFunction();
     console.log("myname");
 }catch(error){
-    console.log(error.name);
+    console.log(error.name);// tell you the typr of error occured
+    console.log(error.message);// show u message along with error like here "somefunction not defined"
+    console.log(error.stack);    ///define the full location and descritpion about error
     console.log(error);
+}finally{                            /// this will be execuated every time irrescpective try block or catch block runs
+console.log(" finally executed");
 }
 
 console.log(userName);
---
+++ functions.js
@@ -0,0 +1,23 @@
+/** 
+ * fubctions prevents the number of developers to write the piece of code over and over again 
+ * SYNTAX
+ * function name_of_the_function(){
+ * body
+ * }
+ * 
+ * // calling the function 
+ * name_of_the _function();
+ */
+
+
+// printing the greating message using the function 
+function greatingmessage(){
+    console.log("hello coders... keeping going u all are doing good ");
+}
+const num= 5;
+if(num>=5){
+    greatingmessage();
+}else
+console.log("hello ");
+
+
