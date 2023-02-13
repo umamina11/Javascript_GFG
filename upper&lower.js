47a3e41479df01369e19cfe54e033c77fd86e805
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-02-13 21:58:17 +0530

    uppercase and lower case string method

---
+++ upper&lower.js
@@ -0,0 +1,7 @@
+// Changing the alphabets or string to uper case and the lower cases 
+// using the uppercase() and the lowercase() methods
+
+const message=" i am here to learn javascript";
+console.log(message.toLowerCase());
+console.log(message)
+console.log(message.toUpperCase());
