0fc8adb1d38b0a8eb6e9eb412ace57c6d6ef195a
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-02-15 15:32:39 +0530

    switch statement

---
+++ switch.js
@@ -0,0 +1,14 @@
+//Given a number, you have to use switch statement to print "One" (without quotes) if the given number is equal to 1 else print "Not One"(without quotes).
+
+//Your Task: 
+//Just write a switch statement at the mentioned place that prints "One" if the given number is equal to 1 else prints "Not One".
+
+let number = 1;
+
+switch (number) {
+  case 1:
+    console.log("One");
+    break;
+  default:
+    console.log("Not One");
+}
