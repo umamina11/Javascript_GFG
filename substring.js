870abe0220ccca53538b3876d66780650a42460e
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-02-15 18:44:25 +0530

    String method- substring

---
+++ substring.js
@@ -0,0 +1,10 @@
+// STRING METHOD 
+
+
+const text="hello myslef uma minaa ";
+
+const subString=text.substring(0,13);             // it will take from index 0 to ondex 10
+
+console.log(subString);
+
+console.log(subString+" Neha mina")
