2cff42762c191ea798047f1a029b3f111bb59f46
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-02-16 10:34:49 +0530

    trim STRING METHOD

---
+++ trim.js
@@ -0,0 +1,9 @@
+// trim- string method 
+//String.trim()
+
+ const text ="             hii its me.           ";     // the wide space before stating the sentneces is called leading and the wide space after completeting if the senstence is called trailing 
+
+ console.log(text.length);   // it will counts the spaces in the total length of the  text  
+console.log(text); //
+
+ console.log(text.trim);
