616641df516d20405b6b8663655e9853a2a85e85
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-02-26 20:05:18 +0530

    Update copref2.js

--- copref2.js
+++ copref2.js
@@ -1,12 +1,3 @@
 // prinitng the two arrays same using the copy by reference command and the putting a 
 
-let data= 7;
-while (data>=0)
-{
-    if(data<=5){
-        data--;
-        continue;
-    }
-    console.log(data);
-    data--;
-}
+let arr4=[1,2,3,4];
