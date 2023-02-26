4b3aeef34448c08130fb744d3d7cf0a2d641b66d
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-02-26 20:24:06 +0530

    spread operator

--- copref2.js
+++ copref2.js
@@ -1,3 +1,21 @@
 // prinitng the two arrays same using the copy by reference command and the putting a 
+// spread operator : which works in array where it creates and array assign memeory and them make updates in particular that new array created 
 
 let arr4=[1,2,3,4];
+let arr5=[];
+
+for(let number of arr4){
+    arr5.push(number);
+}
+console.log("arr4 ",arr4);
+console.log("arr5 ",arr5);
+arr5.push(7);
+
+console.log("updated arr4 ",arr4);
+console.log("updated arr5 ",arr5);
+
+
+// another example with string 
+let str="umamina";
+let arrname=[...str];
+console.log("arrname ",arrname);
