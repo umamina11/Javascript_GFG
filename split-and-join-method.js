815101c321dcd6d24d66008bc5b2459d51de81f8
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-02-28 22:20:30 +0530

    update

--- split-and-join-method.js
+++ split-and-join-method.js
@@ -5,17 +5,20 @@
 
 // best exmaple is plandrome: which is same if read from front or end: madam
 
-const inputStr="Madam";
+const inputStr="madam";
 const arr=[1,2,3,4];
 console.log(arr.reverse());
 
 const arrOfChar=inputStr.split(""); // in split we need to defined the method through which we need to split the array like after ech letter or after each spce or whatever 
                                      //by default the array got split with spaces 
                                     // convert into array
-arrOfChar.reverse();
+ arrOfChar.reverse();
 
 const reversedStr=arrOfChar.join("");    //join: covert back into string
 
 console.log(arrOfChar);
 
 console.log(reversedStr);
+
+const reversedStr2=arrOfChar.join(","); // can join array with characeters between them
+console.log(reversedStr2);
