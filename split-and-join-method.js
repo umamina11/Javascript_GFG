2f370b5bd3da846e1e3e73781013627ab8cb794a
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-02-28 22:16:05 +0530

    Split and Join methods

---
+++ split-and-join-method.js
@@ -0,0 +1,21 @@
+// array methods: split and join
+//split: method of string
+//join method of array
+//both work in cojunction 
+
+// best exmaple is plandrome: which is same if read from front or end: madam
+
+const inputStr="Madam";
+const arr=[1,2,3,4];
+console.log(arr.reverse());
+
+const arrOfChar=inputStr.split(""); // in split we need to defined the method through which we need to split the array like after ech letter or after each spce or whatever 
+                                     //by default the array got split with spaces 
+                                    // convert into array
+arrOfChar.reverse();
+
+const reversedStr=arrOfChar.join("");    //join: covert back into string
+
+console.log(arrOfChar);
+
+console.log(reversedStr);
