aeff11e214fd873c08e4b8105ab48d35b7e573a9
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-02-28 22:31:03 +0530

    spread array

---
+++ spread.js
@@ -0,0 +1,11 @@
+//spread method
+
+const arr=[1,2,3,4,5];
+
+console.log(arr);
+console.log(...arr);     // spreading the array that measn unpacking the array
+                         // it avoid mutation (not changing the array)
+
+const arr1=[2,4,6,8];
+const arr2=[...arr,...arr1];
+console.log(arr2);
