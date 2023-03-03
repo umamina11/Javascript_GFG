49d1e59ce5cdc1198aade071185e98e763ef6b63
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-03 22:34:53 +0530

    adding property to function

---
+++ computedprop.js
@@ -0,0 +1,16 @@
+// adding property to object
+//computing property
+
+
+
+const readlineSync=require("readline-sync")
+
+const obj={
+name: "uma mina",
+age: 21
+}
+
+obj.city="Jaipur";
+obj.state="Rajasthan";
+
+console.log(obj);
