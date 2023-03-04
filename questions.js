1886c780e422545e67b3b064c4932271860a4e3d
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-05 00:04:53 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -11,4 +11,33 @@ GFG.pop();
 console.log(GFG.length);
 
 //3
-'; '
+let temp=["Geeks","For","Geeks"]
+for(let i=0;i<temp;i++){
+    if(temp[i]=="Geeks"){
+        console.log(i);
+    }
+}
+
+//4
+const arr=[1,2,3,4,5];
+console.log(...arr);
+
+//5
+
+const firstName='Rishi'
+lastName='Raj'
+
+console.log('hello ${firstName},nice to meet you');
+
+//6
+
+const str1='Geeksfor'
+const str2='Geeks'
+const str3=str1.concat(str2);
+console.log(str3);
+
+//7
+
+const str11='GeeksforGeeks';
+const str12=str11.slice(2,5);
+console.log(str12);
