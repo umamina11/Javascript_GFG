5b07e8447c2780e0006f159f4fdcf57e11000467
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-04 23:12:36 +0530

    geeks for geeks snippets output

---
+++ questions.js
@@ -0,0 +1,14 @@
+//1
+// output of the snippets
+
+let text="hello world, welcome to the universe";
+let result=text.includes("world");
+console.log(result);
+
+//2
+const GFG=["geeks","for","geeks"];
+GFG.pop();
+console.log(GFG.length);
+
+//3
+'; '
