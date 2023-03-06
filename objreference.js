f9bcadb70cf76444232deb679a1b322d8ac23ddd
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-06 22:33:47 +0530

    Update objreference.js

--- objreference.js
+++ objreference.js
@@ -18,4 +18,27 @@ person2.name="Neha ";
 
  //**objects are not copies by reference but by reference
 
- 
+
+// deep copy to deal with the shallow copy 
+//where change in one onject will make change in rest of its objetcs 
+// like the above scenario
+
+
+const member1={
+    name:"umamina",
+    age: 21,
+    email: "umamina11@gamil.com",
+    address:{
+        city: "jaipur",
+        state: "rajasthan"
+    }
+}
+const member2= Object.assign({},member1);
+member2.name="neha";
+console.log("updated ",member1);
+console.log("updated ",member2);
+
+member2.address.city="pune";
+
+console.log("city updates",member2);
+console.log(member1);
