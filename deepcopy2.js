3fa1ff2405a1d8fd0bf4f2faf6ef83f73dceb218
umamina11 <62588358+umamina11@users.noreply.github.com>
2023-03-07 22:59:24 +0530

    Create deepcopy2.js

---
+++ deepcopy2.js
@@ -0,0 +1,41 @@
+
+// deep copy by creating a function 
+
+const obj={
+    name: "Uma Mina",
+    age: 21,
+    address:{
+        city: "Jaipur",
+        state: "Rajasthan"
+    }
+}
+console.log(Object.keys(obj));
+//adding an key and value to the object in different ways 
+//1
+obj.pinCode="302033";
+//2
+obj["pinCode"]="302033";
+//3
+let key="pinCode";
+obj[key]="302033";
+
+console.log(obj);
+
+
+// funciton to make a copied object 
+function getCopiedObjcet(source){
+ const targetObj={};
+ const keys=Object.keys(source);
+ for(let key of keys){
+ //   targetObj[key]=source[key]
+if(typeof source[key]==="object"){
+    
+}
+ }
+ return targetObj;
+}
+const copiedObj=getCopiedObjcet(obj);
+
+copiedObj.name="Neha Mina"
+console.log("original object",obj)
+console.log("new copied object",copiedObj);
